f41b5727894e4163c52a24d00ea614075ecf2b1d
Jamesamoan <jamessamoani@gmail.com>
2026-07-31 17:20:01 +0300

    initial commit

---
+++ Wel.py
@@ -0,0 +1,16 @@
+#print("Welcome to the world of programming")
+#print(6)
+#print("hey my name is jeff \nIn coding")
+#print("code",7)
+#print("thank you",end="!")
+import keyword
+print("Python keywords are .../n")
+print(keyword.kwlist)
+number=5
+name="John"
+print(name)
+print(number)
+heads="Codingal"
+print(heads)
+str=input("Enter your name")
+print("hello",str,"\n Welcome to Codingal")

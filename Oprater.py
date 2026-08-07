eb54fee26b0c2b8e20644447f74d4df324d978d3
Jamesamoan <jamessamoani@gmail.com>
2026-08-07 17:25:01 +0300

    initial commit

---
+++ Oprater.py
@@ -0,0 +1,26 @@
+f1=28 
+f2=150
+f3=298
+f4=50
+f5=167
+total=f1+f2+f3+f4+f5
+print("Total is:" ,total)
+average=total/5
+print("Average is:",average)
+price=15
+earnings=total*price
+print("Total earnings:",earnings)
+Bags=total//25
+print("Bags:",Bags)
+leftovers=total%25
+print("Leftover:",leftovers)
+last=500
+print("Better than last Year?" ,total>last)
+print("Same as last Year?" ,total==last)
+print("As good as last Year?" ,total>=last)
+total+=30
+print("The Bonus crop is:",total)
+total-=15
+print("Total Seeds:",total)
+bags=total//25
+print("Final Bag Count is:",bags)

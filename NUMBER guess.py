8a411843df058b908ccc932fcffb5527179f2d65
Jamesamoan <jamessamoani@gmail.com>
2026-09-18 19:39:44 +0300

    initial commirt

---
+++ NUMBER guess.py
@@ -0,0 +1,15 @@
+
+secret       = 27      
+max_attempts = 5
+
+count = 0
+guess = 0
+
+print("=" * 42)
+print("        🎮  NUMBER GUESSING GAME")
+print("=" * 42)
+print("I have a secret number between 1 and 50.")
+print("You have 5 attempts to guess it.")
+print("After each wrong guess I will give you a hint.")
+print()
+
